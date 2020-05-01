cb04e27b705deb3588feb54e114771ecde1db729
Anan-Ashraf <64666474+Anan-Ashraf@users.noreply.github.com>
2020-05-02 01:41:38 +0200

    Add files via upload

---
+++ controller.h
@@ -0,0 +1,17 @@
+#ifndef     _M_H
+#define     _M_H
+
+#define     dirB0     TrisB.b0
+#define     dirB2     TrisB.b2
+#define     dirB4     TrisB.b4
+
+#define     valB0     portB.b0
+#define     valB2     portB.b2
+#define     valB4     portB.b4
+
+
+#define      high      1
+#define      low       0
+#define      input     1
+#define      output    0
+#endif
